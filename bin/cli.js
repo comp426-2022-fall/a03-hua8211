aa9e568e1cfcd47baa0a425d96b5b1e5265c676b
Hua Jiang <huahua@email.unc.edu>
2022-11-17 14:55:07 -0500

    cli.js 7

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
 import {roll} from "../lib/roll.js"
+import parseArgs from 'minimist';
 
-import minimist from 'minimist'
+var argv = parseArgs(process.argv);
+var r = roll(argv.sides, argv.dice, argv.rolls);
 
-const args = minimist(process.argv.slice(2))
-
-let sides = args.sides && args.sides > 0 ? args.sides: 6;
-let dice = args.dice && args.dice>0 ? args.dice: 2;
-let rolls = args.rolls && args.rolls>0 ? args.rolls: 1;
-
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+const obj = {sides: argv.sides, dice: argv.dice, rolls: argv.rolls, results: r};
+console.log(JSON.stringify(obj));
